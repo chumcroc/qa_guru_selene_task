c4fb6df737b2323dc480146b32724662b6ad68f6
aleksejartemov <chumcroc@gmail.com>
2022-12-02 22:55:48 +0300

    Поменял стрчоки кода местами

--- conftest.py
+++ conftest.py
@@ -5,6 +5,6 @@ from selene import be, have
 
 @pytest.fixture()
 def open_browser():
-    browser.open('https://google.com')
     browser.config.window_width = 1000
     browser.config.window_height = 1200
+    browser.open('https://google.com')
