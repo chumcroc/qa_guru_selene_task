4a0420927f0bb2c98c5aae64af5edebae8f5df0a
aleksejartemov <chumcroc@gmail.com>
2022-12-02 22:56:29 +0300

    Почистил код от мусора

--- test_google_finds_selene.py
+++ test_google_finds_selene.py
@@ -4,23 +4,14 @@ from selene import be, have
 
 
 def test_google_search_true(open_browser):
-    """Вводим переменные pos_search_in в кач-ве строки ввода и pos_search_in к кач-ве искомой строки"""
-
     pos_search_in = 'selene'
     pos_search_out = 'yashaka/selene: User-oriented Web UI browser tests in Python'
-
-    """С помощью selenium при запуске браузера в поисковике ищется переменная pos_search_in,
-    после исполняется вторая строка кода, кот-я ищет на страницу переменную pos_search_out"""
-
     browser.element('[name="q"]').should(be.blank).type(pos_search_in).press_enter()
     browser.element('[id="search"]').should(have.text(pos_search_out))
 
 
 def test_google_search_false(open_browser):
-    """Вводим переменную neg_search со значением 'dsadsadadsadx'"""
-
     neg_search = 'ajdasjadsjkdsakld dsaldjsald ld alda jsakd'
     neg_search_result = f'Your search - {neg_search} - did not match any documents.'
-
     browser.element('[name="q"]').should(be.blank).type(neg_search).press_enter()
     browser.element('[id="rcnt"]').should(have.text(neg_search_result))
