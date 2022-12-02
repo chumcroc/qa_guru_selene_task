59dd5161870e4e90ce05e3048c3d16ce28a61b19
aleksejartemov <chumcroc@gmail.com>
2022-12-02 22:51:37 +0300

    Сделал побольше размер браузера

--- conftest.py
+++ conftest.py
@@ -5,6 +5,6 @@ from selene import be, have
 
 @pytest.fixture()
 def open_browser():
-    browser.config.window_width = 600
-    browser.config.window_height = 800
     browser.open('https://google.com')
+    browser.config.window_width = 1000
+    browser.config.window_height = 1200
